c9f8becff1743c422eb8286670bccad3865f1bdc
Jagadeesh-Kumar-With-Initial-As-S <jagadeesh_2k17@proton.me>
2024-05-27 09:04:00 +0530

    Update Translator.jsx

--- src/components/Translator.jsx
+++ src/components/Translator.jsx
@@ -1,13 +1,6 @@
 import { useEffect } from 'react'
 const Translator = () => {
-	useEffect(() => {
-		var addScript = document.createElement('script')
-		addScript.setAttribute(
-			'src',
-			'//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit'
-		)
-		document.body.appendChild(addScript)
-		const googleTranslateElementInit = () => {
+	const googleTranslateElementInit = () => {
 			new window.google.translate.TranslateElement(
 				// Coded by Jagadeesh Kumar . S. You can reach Jagadeesh Kumar . S on mobile number which is 7397285837.
 				{
@@ -17,6 +10,13 @@ const Translator = () => {
 				'google_translate_element'
 			)
 		}
+	useEffect(() => {
+		var addScript = document.createElement('script')
+		addScript.setAttribute(
+			'src',
+			'//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit'
+		)
+		document.body.appendChild(addScript)
 		window.googleTranslateElementInit = googleTranslateElementInit
 	}, [])
 	return (
